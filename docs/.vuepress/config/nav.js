de90769d34220dccc3e8b34ed8855bdd01a9679f
xiaosong <1178807588@qq.com>
2021-11-26 20:32:32 +0800

    add html

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,45 +1,125 @@
 module.exports = [
   { text: "首页", link: "/" },
   {
-    text: "指南",
-    link: "/pages/a2f161/",
+    text: "知识库",
+    link: "/pages/3f741b/",
     items: [
-      { text: "主题初衷与诞生", link: "/pages/52d5c3/" },
-      { text: "介绍", link: "/pages/a2f161/" },
-      { text: "快速上手", link: "/pages/793dcb/" },
-      { text: "目录结构", link: "/pages/2f674a/" },
-      { text: "核心配置和约定", link: "/pages/33d574/" },
-      { text: "自动生成front matter", link: "/pages/088c16/" },
-      { text: "Markdown 容器", link: "/pages/d0d7eb/" },
-      { text: "Markdown 中使用组件", link: "/pages/197691/" },
       {
-        text: "相关文章",
-        items: [
-          { text: "如何让你的笔记更有表现力", link: "/pages/dd027d/" },
-          { text: "批量操作front matter工具", link: "/pages/2b8e22/" },
-          { text: "部署", link: "/pages/0fc1d2/" },
-          { text: "关于写文章和H1标题", link: "/pages/9ae0bd/" },
-          { text: "关于博客搭建与管理", link: "/pages/26997d/" },
-          { text: "在线编辑和新增文章的方法", link: "/pages/c5a54d/" },
-        ],
+        text: "HTML",
+        link: "/pages/56881f/",
+      },
+      {
+        text: "CSS",
+        link: "/pages/3f741b",
+      },
+      {
+        text: "JavaScript",
+        link: "/page/3f741b",
+      },
+      {
+        text: "Vue",
+        link: "/page/3f741b",
+      },
+      {
+        text: "React",
+        link: "/page/3f741b",
+      },
+      {
+        text: "计算机网络",
+        link: "/page/3f741b",
+      },
+      {
+        text: "浏览器原理",
+        link: "/page/3f741b",
+      },
+      {
+        text: "性能优化",
+        link: "/page/3f741b",
+      },
+      {
+        text: "NodeJS",
+        link: "/page/3f741b",
       },
     ],
   },
   {
-    text: "配置",
-    link: "/pages/a20ce8/",
+    text: "算法",
+    link: "/page/3f741b",
     items: [
-      { text: "主题配置", link: "/pages/a20ce8/" },
-      { text: "首页配置", link: "/pages/f14bdb/" },
-      { text: "front matter配置", link: "/pages/3216b0/" },
-      { text: "目录页配置", link: "/pages/54651a/" },
-      { text: "添加摘要", link: "/pages/1cc523/" },
-      { text: "修改主题颜色和样式", link: "/pages/f51918/" },
-      { text: "评论栏", link: "/pages/ce175c/" },
+      {
+        text: "字符串",
+        link: "/page/3f741b",
+      },
+      {
+        text: "数组",
+        link: "/page/3f741b",
+      },
+      {
+        text: "栈",
+        link: "/page/3f741b",
+      },
+      {
+        text: "链表",
+        link: "/page/3f741b",
+      },
+      {
+        text: "队列",
+        link: "/page/3f741b",
+      },
+      {
+        text: "树",
+        link: "/page/3f741b",
+      },
+      {
+        text: "动态规划",
+        link: "/page/3f741b",
+      },
+      {
+        text: "回溯",
+        link: "/page/3f741b",
+      },
     ],
   },
-  { text: "插件", link: "/pages/db78e2/" },
-  { text: "案例", link: "/pages/5d571c/" },
+  // {
+  //   text: "指南",
+  //   link: "/pages/a2f161/",
+  //   items: [
+  //     { text: "主题初衷与诞生", link: "/pages/52d5c3/" },
+  //     { text: "介绍", link: "/pages/a2f161/" },
+  //     { text: "快速上手", link: "/pages/793dcb/" },
+  //     { text: "目录结构", link: "/pages/2f674a/" },
+  //     { text: "核心配置和约定", link: "/pages/33d574/" },
+  //     { text: "自动生成front matter", link: "/pages/088c16/" },
+  //     { text: "Markdown 容器", link: "/pages/d0d7eb/" },
+  //     { text: "Markdown 中使用组件", link: "/pages/197691/" },
+  //     {
+  //       text: "相关文章",
+  //       items: [
+  //         { text: "如何让你的笔记更有表现力", link: "/pages/dd027d/" },
+  //         { text: "批量操作front matter工具", link: "/pages/2b8e22/" },
+  //         { text: "部署", link: "/pages/0fc1d2/" },
+  //         { text: "关于写文章和H1标题", link: "/pages/9ae0bd/" },
+  //         { text: "关于博客搭建与管理", link: "/pages/26997d/" },
+  //         { text: "在线编辑和新增文章的方法", link: "/pages/c5a54d/" },
+  //       ],
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: "配置",
+  //   link: "/pages/a20ce8/",
+  //   items: [
+  //     { text: "主题配置", link: "/pages/a20ce8/" },
+  //     { text: "首页配置", link: "/pages/f14bdb/" },
+  //     { text: "front matter配置", link: "/pages/3216b0/" },
+  //     { text: "目录页配置", link: "/pages/54651a/" },
+  //     { text: "添加摘要", link: "/pages/1cc523/" },
+  //     { text: "修改主题颜色和样式", link: "/pages/f51918/" },
+  //     { text: "评论栏", link: "/pages/ce175c/" },
+  //   ],
+  // },
+  // { text: "插件", link: "/pages/db78e2/" },
+  // { text: "案例", link: "/pages/5d571c/" },
   // { text: '问答', link: '/pages/9cc27d/' },
   // { text: '💖支持', link: '/pages/1b12ed/' },
   // {
